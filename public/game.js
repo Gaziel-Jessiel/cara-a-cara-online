3cc55267acb6836b0d4a29e89f26ef1f8cc42fab
Gabriel Henrique Alves <gabrielh@ufrrj.br>
2026-06-22 00:36:25 -0300

    adicionando tabuleiro

--- public/game.js
+++ public/game.js
@@ -1,38 +1,99 @@
 const socket = io("https://cara-a-cara-online-zikq.onrender.com");
 
-socket.on("connect", () => {
-    console.log("CONECTADO NO SERVIDOR:", socket.id);
-});
-
 const statusDiv = document.getElementById("status");
 
-function createRoom() {
-    const roomId = Math.random().toString(36).substring(2, 8);
+let currentRoom = "";
+let characters = [];
 
-    console.log("criando sala:", roomId);
+socket.on("connect", () => {
+    console.log("Conectado:", socket.id);
+});
+
+function createRoom() {
+
+    const roomId =
+        Math.random().toString(36).substring(2,8);
+
+    currentRoom = roomId;
 
     socket.emit("createRoom", roomId);
 
-    document.getElementById("status").innerText =
+    statusDiv.innerText =
         "Sala criada: " + roomId;
+
+    document.getElementById("roomInput").value =
+        roomId;
 }
 
 function joinRoom() {
-    const roomId = document.getElementById("roomInput").value;
+
+    const roomId =
+        document.getElementById("roomInput").value;
+
+    currentRoom = roomId;
+
     socket.emit("joinRoom", roomId);
-    statusDiv.innerText = "Entrando na sala...";
+
+    statusDiv.innerText =
+        "Entrando na sala...";
 }
 
-socket.on("roomCreated", (roomId) => {
-    document.getElementById("roomInput").value = roomId;
-    statusDiv.innerText = "Você criou a sala: " + roomId;
+socket.on("gameStart", (data) => {
 
+    characters = data.characters;
+
+    document.getElementById("gameArea").style.display =
+        "block";
+
+    renderBoard();
 });
 
-socket.on("playerJoined", (data) => {
-    statusDiv.innerText = `Jogador conectado! Total na sala: ${data.players}`;
-});
+function renderBoard(){
 
-socket.on("errorMessage", (msg) => {
-    statusDiv.innerText = "Erro: " + msg;
+    const board =
+        document.getElementById("board");
+
+    board.innerHTML = "";
+
+    characters.forEach(character => {
+
+        const div =
+            document.createElement("div");
+
+        div.className = "card";
+
+        div.innerHTML = `
+            <b>ID ${character.id}</b><br>
+            ${character.gender}<br>
+            ${character.skin}<br>
+            ${character.hair}<br>
+            ${character.eyes}
+        `;
+
+        div.onclick = () => {
+            div.classList.toggle("eliminated");
+        };
+
+        board.appendChild(div);
+    });
+}
+
+function sendQuestion(){
+
+    const question =
+        document.getElementById("questionInput").value;
+
+    socket.emit("askQuestion", {
+        roomId: currentRoom,
+        question
+    });
+}
+
+socket.on("question", (data) => {
+
+    const messages =
+        document.getElementById("messages");
+
+    messages.innerHTML +=
+        `<p><b>Pergunta:</b> ${data.question}</p>`;
 });
